179069e711407cf1dabac96edad6f5b18af5d480
Jasmine Drumright <jmdrumright@gmail.com>
2022-03-05 13:59:12 -0600

    Stuck on 5 need to use Teams instead of Batting

--- lahman_baseball_jd.sql
+++ lahman_baseball_jd.sql
@@ -101,5 +101,11 @@ FROM fielding
 WHERE yearid = '2016'
 GROUP BY position;
 
--- Q5: Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
+-- Q5: Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends? USE THE TEAMS TABLE
 
+SELECT yearid/10*10 AS decade,
+	AVG(so)
+FROM batting
+WHERE yearid >= '1920'
+GROUP BY yearid/10*10
+ORDER BY decade DESC;
