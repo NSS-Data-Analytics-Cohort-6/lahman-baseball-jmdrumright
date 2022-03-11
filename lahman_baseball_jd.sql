02902b8beae8cea87e5f9d5b1d04255a46b5a415
Jasmine Drumright <jmdrumright@gmail.com>
2022-03-10 21:27:40 -0600

    Reviewed all Qs with two groups maybe redo 9 into one

--- lahman_baseball_jd.sql
+++ lahman_baseball_jd.sql
@@ -10,7 +10,7 @@ SELECT
 	MIN(yearid),
 	MAX(yearid)
 FROM collegeplaying;
--- 1864 to 2014, use MIN
+-- 1864 to 2014, use MIN (Rob)
 
 SELECT
 	MIN(c.yearid),
@@ -81,6 +81,7 @@ ORDER BY p.height;
 -- Q3: Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 
 SELECT
+	p.playerid,
 	CONCAT(CAST(p.namefirst AS text), ' ', CAST(p.namelast AS text)) AS full_name,
 	s.schoolname,
 	SUM(sa.salary) AS total_salary_earned
@@ -92,11 +93,31 @@ ON c.schoolid = s.schoolid
 JOIN salaries AS sa
 ON p.playerid = sa.playerid
 WHERE s.schoolname = 'Vanderbilt University'
-GROUP BY full_name,
+GROUP BY p.playerid,
+	full_name,
 	s.schoolname
-ORDER BY total_salary_earned DESC;
--- A3: David David Taylor Price at $245,553,888.
+ORDER BY total_salary_earned DESC; -- A3: David Price at $245,553,888.
 
+SELECT playerid, SUM(salary)
+FROM salaries
+WHERE playerid LIKE 'priceda01'
+GROUP BY playerid -- David Price should be $81,851,596????
+
+SELECT
+	DISTINCT p.playerid,
+	s.salary,
+	SUM(s.salary) AS sumsalary,
+	s.yearid
+FROM people AS p
+LEFT JOIN collegeplaying AS c
+ON p.playerid = c.playerid
+LEFT JOIN salaries AS s
+ON p.playerid = s.playerid
+WHERE c.playerid IN (SELECT playerid
+				  FROM collegeplaying
+				  WHERE schoolid LIKE 'vandy') -- This shows that David Price should be $245,553,888 tho.
+	AND c.playerid LIKE 'priceda01'
+GROUP BY p.playerid, s.salary, s.yearid
 -- Q4: Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
 
 SELECT
@@ -145,8 +166,30 @@ SELECT
 FROM teams
 WHERE yearid >= '1920'
 GROUP by decade
-ORDER BY decade DESC;
--- A5: Strikeouts more common in the latter half of 1920-2010
+ORDER BY decade;
+
+-- Katie A5:
+SELECT 
+	ROUND((ROUND(AVG(so),2) / (SUM(g) / 2)), 2) AS avgsopg,
+	ROUND((ROUND(AVG(hr),2) / (SUM(g) / 2)), 2) AS avghrpg,
+	CASE WHEN yearid BETWEEN 1920 AND 1929 THEN '1920s'
+		 WHEN yearid BETWEEN 1930 AND 1939 THEN '1930s'
+		 WHEN yearid BETWEEN 1940 AND 1949 THEN '1940s'
+		 WHEN yearid BETWEEN 1950 AND 1959 THEN '1950s'
+		 WHEN yearid BETWEEN 1960 AND 1969 THEN '1960s'
+		 WHEN yearid BETWEEN 1970 AND 1979 THEN '1970s'
+		 WHEN yearid BETWEEN 1980 AND 1989 THEN '1980s'
+		 WHEN yearid BETWEEN 1990 AND 1999 THEN '1990s'
+		 WHEN yearid BETWEEN 2000 AND 2009 THEN '2000s'
+		 WHEN yearid BETWEEN 2010 AND 2019 THEN '2010s' 
+		 END AS decade
+/* Alternative: CONCAT(LEFT(CAST(yearid AS text), 3), '0s') AS decade */
+FROM teams
+WHERE yearid >= 1920
+GROUP BY decade
+ORDER BY decade;
+
+-- A5: Strikeouts peaked in the mid 1900s
 
 -- Average number of home runs per game by decade since 1920:
 
@@ -253,6 +296,43 @@ ORDER BY yearid DESC;
 
 -- A7: 12 / 47 of the years from 1970 to 2016 (inclusive) or 25.53% of the time.
 
+-- Abigail A7:
+WITH top_scores AS
+	(SELECT a.yearid,
+		a.name,
+		a.w,
+		a.wswin
+	FROM teams AS a
+	INNER JOIN (
+		SELECT yearid,
+				MAX(w) AS w
+		FROM teams
+		GROUP BY yearid
+		ORDER BY yearid) AS b
+	ON a.yearid = b.yearid AND a.w = b.w
+	WHERE a.yearid BETWEEN 1970 AND 2016)
+	SELECT SUM(CASE WHEN wswin = 'Y' THEN 1
+			   WHEN wswin = 'N' THEN 0 END) AS total -- This matches my 12
+		FROM top_scores;
+
+WITH top_scores AS
+	(SELECT a.yearid,
+		a.name,
+		a.w,
+		a.wswin
+	FROM teams AS a
+	INNER JOIN (
+		SELECT yearid,
+				MAX(w) AS w
+		FROM teams
+		GROUP BY yearid
+		ORDER BY yearid) AS b
+	ON a.yearid = b.yearid AND a.w = b.w
+	WHERE a.yearid BETWEEN 1970 AND 2016)
+	SELECT ROUND(AVG(CASE WHEN wswin = 'Y' THEN 1
+			   WHEN wswin = 'N' THEN 0 END)*100, 2) AS avg
+		FROM top_scores; -- Denominator seems to be 52 (12/52) so her perc is 23.08%
+
 -- Q8: Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
 
 SELECT
@@ -323,6 +403,7 @@ am AS (
 		AND lgid LIKE 'AL')
 SELECT
 	nat.playerid,
+	p.namegiven,
 	p.namefirst,
 	p.namelast,
 	nat.yearid AS NLwinningyear,
@@ -364,31 +445,26 @@ ORDER BY m.yearid DESC;
 
 -- Q10: Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
 
-/* ALTERNATIVE maxhr CTE: Running this also returns 81 players in the end.
-WITH maxhr AS ( -- 1st CTE: Highest hr for each player out of the entirety of the table
+WITH maxhr AS ( -- 1st CTE: Max hr of each player's annual hr
 	SELECT
-		DISTINCT playerid,
-		MAX(hr) OVER(PARTITION BY playerid) AS maxhrpp
-	FROM batting
-	GROUP BY playerid, hr),*/
-
-WITH maxhr AS ( -- 1st CTE: Max of the max hr per year for each player
-	SELECT
-	playerid,
-	yearid,
-	MAX(maxhrpp) OVER(PARTITION BY playerid) AS maxmaxhrpp -- It's giving the same career high hr value for each career year but I want just the one year
+		playerid,
+		yearid,
+		MAX(maxhrppby) OVER(PARTITION BY playerid) AS maxmaxhrppby -- Does yearid even matter?
 	FROM(
-			SELECT
-				playerid,
-				yearid,
-				SUM(hr) OVER(PARTITION BY playerid, yearid) AS maxhrpp -- Gimme the sum of the player then of the years within that player
-			FROM batting
-			GROUP BY playerid, yearid, hr) AS subquery
+		SELECT DISTINCT
+			playerid,
+			yearid,
+			SUM(hr) OVER(PARTITION BY playerid, yearid) AS maxhrppby -- Gimme each player's annual hr (dup years removed w DISTINCT)
+		FROM batting
+		GROUP BY playerid, yearid, hr
+		ORDER BY playerid) AS subquery
 	GROUP BY
 		playerid,
 		yearid,
-		maxhrpp
-	ORDER BY maxmaxhrpp DESC, yearid DESC),
+		maxhrppby
+	ORDER BY playerid, yearid DESC),
+--NO: If their maxmaxhrppby (not 2016) > 2016 hr, then they did NOT hit their career highest hr in 2016.
+--YES: If their maxmaxhrppby (2016) <= 2016 hr, then they DID hit their career highest hr in 2016.
 
 sixteenhr AS ( -- 2nd CTE: Number of homeruns for each player in 2016 with at least 1 hr
 	SELECT
@@ -402,17 +478,15 @@ sixteenhr AS ( -- 2nd CTE: Number of homeruns for each player in 2016 with at le
 decade AS ( -- 3rd CTE: Players with at least 10 yrs in the league
 	SELECT
 		b.playerid,
-		b.hr,
 		p.debut,
 		p.finalgame,
 		CAST(p.finalgame AS date) - CAST (p.debut AS date) AS daysinlg
 	FROM batting AS b
 	JOIN people AS p
 	ON b.playerid = p.playerid
-	WHERE CAST(p.finalgame AS date) - CAST (p.debut AS date) >= 3650 -- Played for at least 10 years
+	WHERE CAST(p.finalgame AS date) - CAST(p.debut AS date) >= 3650 -- Played for at least 10 years
 	GROUP BY
 		b.playerid,
-		b.hr,
 		p.finalgame,
 		p.debut)
 -- Note: The only nulls for p.debut and p.finalgame are when both are null (195 rows) so don't worry about them
@@ -422,7 +496,7 @@ SELECT
 	p.namefirst,
 	p.namelast,
 	mh.yearid,
-	mh.maxmaxhrpp,
+	mh.maxmaxhrppby,
 	sh.totalhrppinsixteen,
 	ROUND((CAST(d.daysinlg AS numeric) / 365), 2) AS yrsinlg,
 	d.daysinlg
@@ -434,7 +508,7 @@ ON sh.playerid = d.playerid
 JOIN people AS p
 ON d.playerid = p.playerid
 WHERE yearid = 2016 -- Show me their hr for 2016 only
-	AND mh.maxmaxhrpp = sh.totalhrppinsixteen -- Show me that their career high matches 2016
+	AND mh.maxmaxhrppby = sh.totalhrppinsixteen -- Show me that their career high matches 2016
 -- A10: 8 players hit their career highest number of home runs in 2016 (verified playerids thru the query below)
 -- Note: Some ppl's career high is in 2016 and another year: Edwin Encarnacion (2012), Francisco Liriano (2015), Adam Wainwright (2009)
 
@@ -444,3 +518,56 @@ WHERE playerid LIKE 'canoro01' -- Change playerid to verify their career max is 
 GROUP BY yearid, playerid, hr
 ORDER BY hr DESC, yearid DESC;
 -- Playerids (8): canoro01, colonba01, davisra01, encared01, liriafr01, napolmi01, paganan01, wainwad01
+
+-- Katie A10 (returns dupes):
+WITH a AS
+	(SELECT 
+		b.playerid,
+		COUNT (b.yearid) AS years
+	FROM batting AS b
+	GROUP BY b.playerid
+	HAVING COUNT(b.yearid)>10),
+
+c AS
+	(SELECT
+		b.playerid,
+	 	b.yearid,
+	 	b.hr,
+		MAX(b.hr) AS career_high
+	  FROM batting AS b
+	  WHERE b.yearid = '2016'
+	  GROUP BY 
+	 	b.playerid,
+		b.yearid,
+	 	b.hr
+	   HAVING b.hr = MAX(b.hr))
+
+SELECT
+ 	a.playerid,
+	a.years,
+	b.hr,
+	c.career_high,
+	p.namefirst,
+	p.namelast,
+	b.yearid,
+	c.yearid
+FROM batting AS b
+INNER JOIN people AS p
+ON b.playerid = p.playerid
+INNER JOIN a
+ON a.playerid = p.playerid
+INNER JOIN c
+ON a.playerid = c.playerid
+WHERE b.yearid = 2016
+	AND b.yearid = c.yearid
+	AND b.hr >= 1
+GROUP BY 
+	a.playerid,
+	a.years,
+	b.hr,
+	c.career_high,
+	p.namefirst,
+	p.namelast,
+	b.yearid,
+	c.yearid
+ORDER BY c.career_high DESC;
